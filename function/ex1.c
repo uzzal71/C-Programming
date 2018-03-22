64c6787f3389736bea927a46f4967989c999b38a
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-22 16:44:40 +0600

    Create message function and print

---
+++ function/ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// function declaration
+void message();
+
+int main()
+{
+	// function calling
+	message();
+	printf ( "\nCry, and you stop the monotony!" ) ;
+	return 0;
+}
+
+
+// Message function definetion
+ message()
+{
+	printf ( "\nSmile, and the world smiles with you..." ) ; 
+}
