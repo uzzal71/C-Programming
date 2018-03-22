fae5f0f859f550f23d79a3b14efc8df3e99a948f
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-22 17:12:54 +0600

    Function call and definition are not ordering

---
+++ function/ex6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//function declaration
+void message1();
+void message2();  
+
+int main()
+{
+	//function calling
+	message1();
+	message2();
+}
+
+//function definition
+message2() {
+	printf ( "Message two\n" ) ; 	
+}
+
+message1() {
+	printf ( "Message one\n" ) ; 	
+}
+
