91cf08f03d34103f6d0908b06f8ebfba9dd87393
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-22 17:08:46 +0600

    A function can be called any number of times

---
+++ function/ex5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//function declaration
+void message(); 
+
+int main()
+{
+	//function calling
+	message();
+	message();
+}
+
+//function definition
+message() {
+	printf ( "\nNorthern University Bangladesh\n" ) ; 	
+}
+
