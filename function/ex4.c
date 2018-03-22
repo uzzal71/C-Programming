24938c0b93e619c093793f03bf3abc4ad87d5d7d
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-22 17:06:16 +0600

    Main function call another function

---
+++ function/ex4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//function declaration
+void message(); 
+
+int main()
+{
+	//function calling
+	message();
+}
+
+//function definition
+message() {
+	printf ( "\nCan't imagine life without C" ) ; 	
+	main();
+}
+
